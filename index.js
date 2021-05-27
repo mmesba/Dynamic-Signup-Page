3d8c1cbc6d0deaa4a38d122f4e882154887149da
Mohammad Mesbaul Haque <mesbaul333@gmail.com>
2021-05-28 03:26:28 +0600

    index.ejs created

---
+++ index.js
@@ -0,0 +1,21 @@
+/*
+ * Title: Dynamic Signup Page
+ * Description: Dynamic Signup Page project's index.js file or entry point.
+ * Author: Mohammad Mesbaul Haque (github profile: https://github.com/Mohammad-Mesbaul-Haque )
+ * Date: 28/05/2021
+ */
+// Dependencies.
+
+
+
+// App object or Module scaffolding.
+
+// Main functions.
+
+
+
+
+
+
+// export the module.
+// module.exports = 
