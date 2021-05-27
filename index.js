9e117f4eaf2624c5767e6466c2735fc33ffe6bf0
Mohammad Mesbaul Haque <mesbaul333@gmail.com>
2021-05-28 04:20:42 +0600

    error handling macanism added

--- index.js
+++ index.js
@@ -12,19 +12,32 @@ const dotenv = require('dotenv');
 // App object or Module scaffolding.
 const app = express();
 
+// Set View engine
+app.set('view engine', 'ejs');
+
 // dotenv permission
 dotenv.config();
 const port = process.env.PORT || 8080;
+
+// initiate static dir
+app.use('/Assets', express.static('./Assets'));
 
 // Main functions.
 
 // Check route
 app.get('/', (req, res)=>{
-    res.send('Check route');
+    res.render('index');
 })
 
 
-
+// App's error handling
+app.use(function(err, req, res, next){
+    if (err) {
+        res.status(500).send(err.message);
+    } else {
+        res.status(500).send('There was an error !');
+    }
+})
 
 // Creating Server
 app.listen(port, ()=>{
