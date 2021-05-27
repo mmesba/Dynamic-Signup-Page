a47411a27ef23e7a99a82a6688e3387787cff97d
Mohammad Mesbaul Haque <mesbaul333@gmail.com>
2021-05-28 03:41:26 +0600

    Server created

--- index.js
+++ index.js
@@ -5,16 +5,31 @@
  * Date: 28/05/2021
  */
 // Dependencies.
-
+const express = require('express');
+const dotenv = require('dotenv');
 
 
 // App object or Module scaffolding.
+const app = express();
+
+// dotenv permission
+dotenv.config();
+const port = process.env.PORT || 8080;
 
 // Main functions.
 
+// Check route
+app.get('/', (req, res)=>{
+    res.send('Check route');
+})
 
 
 
+
+// Creating Server
+app.listen(port, ()=>{
+    console.log(`App running on ${port}`);
+})
 
 
 // export the module.
