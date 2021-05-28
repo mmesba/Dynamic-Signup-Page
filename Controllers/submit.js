6b3ff79a612b94b2507df39b646abc39cb657b52
Mohammad Mesbaul Haque <mesbaul333@gmail.com>
2021-05-28 14:55:27 +0600

    submit file initiated

---
+++ Controllers/submit.js
@@ -0,0 +1,21 @@
+/*
+ * Title: Submit functions
+ * Description: Submit functions for app
+ * Author: Mohammad Mesbaul Haque (github profile: https://github.com/Mohammad-Mesbaul-Haque )
+ * Date: 29/05/2021
+ */
+// Dependencies.
+
+
+
+// App object or Module scaffolding.
+
+// Main functions.
+
+
+
+
+
+
+// export the module.
+// module.exports = 
