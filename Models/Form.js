6f28a5701863e970e059f0f4da7b25e957ee9e4b
Mohammad Mesbaul Haque <mesbaul333@gmail.com>
2021-05-28 16:49:45 +0600

    Form schema and model created

---
+++ Models/Form.js
@@ -0,0 +1,58 @@
+/*
+ * Title: Form Schema.
+ * Description: Form Schema for registration form
+ * Author: Mohammad Mesbaul Haque (github profile: https://github.com/Mohammad-Mesbaul-Haque )
+ * Date: 29/05/2021
+ */
+// Dependencies.
+const mongoose = require('mongoose');
+
+
+// Creating form schema
+const formSchema = mongoose.Schema({
+    firstName: {
+        type: String,
+        required: true
+    },
+    lastName: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    gender: {
+        type: String,
+        enum: ['male, female, other'],
+        required: true
+    },
+    birthday: {
+        type: Date,
+        default: Date.now
+    },
+    password: {
+        type: String,
+        required: true
+    }, 
+    confirmPassword: {
+        type: String,
+        required: true
+    },
+    profilePicture: {
+        type: String,
+        required: true
+    },
+    remember: {
+        type: String
+    }
+})
+
+
+// Creating model
+const FormModel = mongoose.model('User', formSchema);
+
+
+
+// export the module.
+module.exports = FormModel;
